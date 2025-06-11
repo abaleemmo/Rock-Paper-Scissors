8be0b3b7130e4ab0feac171e5f89a864150b91e5
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-11 13:06:56 -0500

    computer choice function made

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let choice;
+
+function getComputerChoice(){
+   choice = Math.floor(Math.random()*3);
+};
+
+getComputerChoice()
+console.log(choice);
+
+if (choice === 0)
+ {
+    console.log("Rock");
+}
+
+if (choice === 1)
+{
+    console.log("Paper");
+}
+
+if (choice === 2)
+{
+    console.log("Scissors")
+}
